dd1198896cbba4ef8931f0534e437f590c049312
RATCHANON CHUNSAKUNEE <kirigayannd6355@gmail.com>
2023-08-10 14:52:55 +0700

    js

---
+++ src/components/userImagePath.jsx
@@ -0,0 +1,12 @@
+export const userImagePath = (props) =>{
+    return(<img
+      src="/profileImages/รัชชานนท์ รูป.jpg"
+      width="48"
+      height="48"
+      className="rounded-circle"
+      style={{ objectFit: "cover" }}
+    />
+
+    );
+
+};
